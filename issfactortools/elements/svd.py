2ea0c7999acd7a7f39262ca4f0c13039505649b2
jlictro01 <jlictro01@manhattan.edu>
2021-06-24 18:49:55 -0400

    Reordered methods so that computational methods are together and graphical methods are together.

--- issfactortools/elements/svd.py
+++ issfactortools/elements/svd.py
@@ -11,28 +11,11 @@ def gaussian(x, amplitude, center, sigma):  # function to calculate a gaussian e
 def exponentialdecay(time, amplitude, tau):  # calculate exponential decay of data
     return amplitude * np.exp(-time / tau)
 
-
-def creategraph(matrix, t, x, y, *args, fig = None):  # takes in matrix, title, xlabel, ylabel, and potenteial args to make plot
-    plt.xlabel(x)
-    plt.ylabel(y)
-    plt.title(t)
-    plt.plot(matrix)
-    if args:  # potential args are number of data points to plot, oriented by colums or rows, and a label
-        num, orientation, lab = args
-        if fig is not None:
-            plt.figure(fig.numner)
-        if (orientation == "c"):
-            i = 0
-            for i in range(num):
-                plt.plot(matrix[:, i], label=(lab + str(i + 1)))
-        elif (orientation == "r"):
-            i = 0
-            for i in range(num):
-                plt.plot(matrix[i, :], label=(lab + str(i + 1)))
-        plt.legend()
-    else:
-        plt.plot(matrix)
-    plt.show()
+def svdnoise(A, noiseLevel, noise):  # pass original matrix A, a noise level, and a noise Matrix
+    # calculate the SVD on matrix A at that noise level
+    noiseA = A + (noiseLevel * noise)
+    u, s, v, = np.linalg.svd(noiseA)
+    return noiseA, u, s, v #return a copy of matrix A with that noise Level applied, and u, s, and v
 
 
 def getrankedmatrices(number, matrix, rcd):  # return a matrix of a specific rank
@@ -48,8 +31,6 @@ def getrankedmatrices(number, matrix, rcd):  # return a matrix of a specific ran
         # print("Diagonal")
         m = np.diag(matrix)
         return m[0:number, 0:number]
-
-
 def getresiduals(uN, sN, vN, A):  # returns matrix A of rank n, and the residual matrix
     An = uN @ sN @ vN  # takes in u, s, and v of rank N, as well as the original matrix, A
     residuals = A - An
@@ -65,22 +46,32 @@ def getchisq(An):  # takes in matrix A of rank N, and calculates chi squared for
     chiSq = sum
     return chiSq
 
-
-def plots(name, matrix): #create a basic graph and imshow plot for a matrix
-    plt.figure()
-    plt.title(name)
-    plt.imshow(matrix)
-    plt.figure()
-    plt.plot(matrix)
+def LRA(uMatrix, sMatrix, vMatrix, n, A, noiseLevel = None, fig = None): # method to calculate LRA
+    # takes in u, s, v, rank n, matrix A
+    # optional paremeters noiseLevel and a figure
+    chiSq = []
+    i = 1
+    while i < n:    # perform LRA for every rank n
+        uN = getrankedmatrices(i, uMatrix, "c")
+        sN = getrankedmatrices(i, sMatrix, "d")
+        vN = getrankedmatrices(i, vMatrix, "r")
+        An, residuals = getresiduals(uN, sN, vN, A)   # calculate residuals
+        chiSq.append(getchisq(residuals)) # calculate chi squared at rank n
+        i = i+1
+    nArr = np.arange(1, len(chiSq)+1) # create an array ranging from 1 to rank n to plot chi squared against
+    if fig is not None:
+        plt.figure(fig.number)
+        plt.plot(nArr, chiSq)
+    else:
+        plt.plot(nArr, chiSq)
+        plt.xlabel("Rank(n)")
+        plt.ylabel("Chi Squared")
+        if noiseLevel is not None:
+            plt.title("Rank vs Chi Squared for a noise level of "+str(noiseLevel))
+        else:
+            plt.title("Rank vs Chi Squared")
     plt.show()
-
-
-def svdnoise(A, noiseLevel, noise):  # pass original matrix A, a noise level, and a noise Matrix
-    # calculate the SVD on matrix A at that noise level
-    noiseA = A + (noiseLevel * noise)
-    u, s, v, = np.linalg.svd(noiseA)
-    return noiseA, u, s, v #return a copy of matrix A with that noise Level applied, and u, s, and v
-
+    plotsingvalues(sMatrix)
 
 def plotsingvalues(s, fig = None): # plot a matrix of singular values
     sMatrix = s
@@ -109,31 +100,38 @@ def plotsingvalues(s, fig = None): # plot a matrix of singular values
         plt.show()
 
 
-def LRA(uMatrix, sMatrix, vMatrix, n, A, noiseLevel = None, fig = None): # method to calculate LRA
-    # takes in u, s, v, rank n, matrix A
-    # optional paremeters noiseLevel and a figure
-    chiSq = []
-    i = 1
-    while i < n:    # perform LRA for every rank n
-        uN = getrankedmatrices(i, uMatrix, "c")
-        sN = getrankedmatrices(i, sMatrix, "d")
-        vN = getrankedmatrices(i, vMatrix, "r")
-        An, residuals = getresiduals(uN, sN, vN, A)   # calculate residuals
-        chiSq.append(getchisq(residuals)) # calculate chi squared at rank n
-        i = i+1
-    nArr = np.arange(1, len(chiSq)+1) # create an array ranging from 1 to rank n to plot chi squared against
-    if fig is not None:
-        plt.figure(fig.number)
-        plt.plot(nArr, chiSq)
+def creategraph(matrix, t, x, y, *args, fig = None):  # takes in matrix, title, xlabel, ylabel, and potenteial args to make plot
+    plt.xlabel(x)
+    plt.ylabel(y)
+    plt.title(t)
+    plt.plot(matrix)
+    if args:  # potential args are number of data points to plot, oriented by colums or rows, and a label
+        num, orientation, lab = args
+        if fig is not None:
+            plt.figure(fig.numner)
+        if (orientation == "c"):
+            i = 0
+            for i in range(num):
+                plt.plot(matrix[:, i], label=(lab + str(i + 1)))
+        elif (orientation == "r"):
+            i = 0
+            for i in range(num):
+                plt.plot(matrix[i, :], label=(lab + str(i + 1)))
+        plt.legend()
     else:
-        plt.plot(nArr, chiSq)
-        plt.xlabel("Rank(n)")
-        plt.ylabel("Chi Squared")
-        if noiseLevel is not None:
-            plt.title("Rank vs Chi Squared for a noise level of "+str(noiseLevel))
-        else:
-            plt.title("Rank vs Chi Squared")
+        plt.plot(matrix)
     plt.show()
-    plotsingvalues(sMatrix)
+
+def plots(name, matrix): #create a basic graph and imshow plot for a matrix
+    plt.figure()
+    plt.title(name)
+    plt.imshow(matrix)
+    plt.figure()
+    plt.plot(matrix)
+    plt.show()
+
+
+
+
 
 
