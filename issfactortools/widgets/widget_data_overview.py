b6ae6fa137fb6cb6e90156e600e9c7889c10981d
jlictro01 <jlictro01@manhattan.edu>
2021-07-21 17:37:00 -0400

    sort by energy or rows/cols added tabbed menu made plots look neater

--- issfactortools/widgets/widget_data_overview.py
+++ issfactortools/widgets/widget_data_overview.py
@@ -7,6 +7,7 @@ import pkg_resources
 import traceback
 import math
 
+import isstools.widgets
 from PyQt5 import uic, QtWidgets, QtGui, QtCore
 from PyQt5.QtCore import QThread, QSettings
 from PyQt5.QtWidgets import QMessageBox, QApplication, QWidget, QPushButton
@@ -28,6 +29,7 @@ class UIDataOverview(*uic.loadUiType(ui_path)):
         self.setupUi(self)
 
         self.addCanvas()
+
         self.dataset = None
 
         self.pushButton_import_data.clicked.connect(self.import_data)
@@ -35,10 +37,12 @@ class UIDataOverview(*uic.loadUiType(ui_path)):
         self.pushButton_display_data.clicked.connect(self.display_data)
 
 
+
+
     def addCanvas(self):
         self.figure_data = Figure()
-        #self.figure_data.set_facecolor(color='#E2E2E2')
-        self.figure_data.ax = self.figure_data.add_subplot(111)
+        self.figure_data.ax = self.figure_data.add_subplot(211)
+        self.figure_data.ax2 = self.figure_data.add_subplot(212)
         self.canvas_data = FigureCanvas(self.figure_data)
         self.toolbar_data = NavigationToolbar(self.canvas_data, self)
         self.toolbar_data.resize(1, 10)
@@ -48,6 +52,10 @@ class UIDataOverview(*uic.loadUiType(ui_path)):
         self.canvas_data.draw()
 
         self.figure_svd= plt.figure()
+        self.figure_svd.ax = self.figure_svd.add_subplot(221)
+        self.figure_svd.ax2 = self.figure_svd.add_subplot(222)
+        self.figure_svd.ax3 = self.figure_svd.add_subplot(223)
+        self.figure_svd.ax4 = self.figure_svd.add_subplot(224)
         self.canvas = FigureCanvas(self.figure_svd)
         self.toolbar = NavigationToolbar(self.canvas, self)
         self.toolbar.resize(1, 10)
@@ -64,44 +72,118 @@ class UIDataOverview(*uic.loadUiType(ui_path)):
         #     filename = filename[:-4]
 
         self.dataset = np.genfromtxt(filename)
+        Myenergy = self.dataset[:, 0]
+        #print(Myenergy)
 
 
+
+    def getFirstLimit(self, num, arr):
+        i = 0
+        fR = 0
+        while i < len(arr):
+            if arr[i] >= num:
+                fR= i
+                return i, fR
+            i = i + 1
+
+    def getSecondLimit(self, num, arr, index):
+        sR = 0
+        i = index
+        while i < len(arr):
+            if arr[i] >= num:
+                sR = i
+                return sR
+            i = i + 1
 
     def display_data(self):
+        components = 3
         if self.dataset is not None:
             rows_text = self.rowsText.toPlainText()
             cols_text = self.columnsText.toPlainText()
-            Myenergy = self.dataset[:, 0]
+            components_text = self.componentsText.toPlainText()
+            components_text2 = self.componentsText2.toPlainText()
+            energy_text = self.energyText.toPlainText()
+            if components_text == "" and components_text2 == "":
+                components = 3
+            elif components_text != "" and components_text2 == "":
+                if int(components_text) <= 0:
+                    QMessageBox.about(self, "ERROR", "The number of components is too low. Retry.")
+                else:
+                    components = int(components_text)
+            elif components_text == "" and components_text2 != "":
+                if int(components_text2) <= 0:
+                    QMessageBox.about(self, "ERROR", "The number of components is too low. Retry.")
+                else:
+                    components = int(components_text2)
+
+
+
+
             self.figure_data.ax.clear()
+
+            Myenergy = self.dataset[:, 0]
+            Mydataset = self.dataset[:, 1:]
+
+            if energy_text == "":
+                Myenergy = self.dataset[:, 0]
+                Mydataset = self.dataset[:, 1:]
+            elif energy_text != "":
+                energy_text = energy_text.split(",")
+                if int(energy_text[0]) < Myenergy[0] or int(energy_text[1]) > Myenergy[len(Myenergy)-1]:
+                    QMessageBox.about(self, "ERROR", "The energy range is invalid. Displaying default range.")
+                else:
+                    i, firstRow = self.getFirstLimit(int(energy_text[0]), Myenergy)
+                    secondRow = self.getSecondLimit(int(energy_text[1]), Myenergy, i)
+                    print("First Row: " + str(firstRow))
+                    print("Second Row: " + str(secondRow))
+                    Mydataset = self.dataset[firstRow:secondRow, 1:]
+                    Myenergy = self.dataset[firstRow:secondRow, 0]
+                    Myenergy = np.array(Myenergy)
+                    print(self.dataset[:,0])
+                    print(Mydataset[1:3, 1:3])
+
             try:
                 if rows_text == "" and cols_text == "":
-                    self.figure_data.ax.plot(self.dataset[:, 0], self.dataset[:, 1:])
-                    #Mydataset = self.dataset[:, 1:]
+                    self.figure_data.ax.plot(Myenergy, Mydataset)
                 elif rows_text == "" and cols_text != "":
                     cols = cols_text.split(",")
-                    self.figure_data.ax.plot(self.dataset[:, 0], self.dataset[:, int(cols[0]):int(cols[1])])
-                    #Mydataset = self.dataset[:, int(cols[0]):int(cols[1])]
+                    if energy_text == "":
+                        self.figure_data.ax.plot(self.dataset[:, 0], self.dataset[:, int(cols[0]):int(cols[1])])
+                    else:
+                        self.figure_data.ax.plot(Myenergy[:, 0], Mydataset[:, int(cols[0]):int(cols[1])])
                 elif rows_text != "" and cols_text == "":
                     rows = rows_text.split(",")
-                    self.figure_data.ax.plot(self.dataset[int(rows[0]):int(rows[1]), 0], self.dataset[int(rows[0]):int(rows[1]), 1:])
-                    #Mydataset = self.dataset[int(rows[0]):int(rows[1]), 1:]
+                    if energy_text == "":
+                        self.figure_data.ax.plot(self.dataset[int(rows[0]):int(rows[1]), 0], self.dataset[int(rows[0]):int(rows[1]), 1:])
+                    else:
+                        self.figure_data.ax.plot(Myenergy[int(rows[0]):int(rows[1]), 0],   Mydataset[int(rows[0]):int(rows[1]), 1:])
                 else:
                     rows = rows_text.split(",")
                     cols = cols_text.split(",")
-                    self.figure_data.ax.plot(self.dataset[int(rows[0]):int(rows[1]), 0], self.dataset[int(rows[0]):int(rows[1]), int(cols[0]):int(cols[1])])
-                    #Mydataset = self.dataset[int(rows[0]):int(rows[1]), int(cols[0]):int(cols[1])]
+                    if energy_text == "":
+                        self.figure_data.ax.plot(self.dataset[int(rows[0]):int(rows[1]), 0], self.dataset[int(rows[0]):int(rows[1]), int(cols[0]):int(cols[1])])
+                    else:
+                        self.figure_data.ax.plot(Myenergy[int(rows[0]):int(rows[1]), 0],  Mydataset[int(rows[0]):int(rows[1]), int(cols[0]):int(cols[1])])
             except Exception as err:
+                print(err)
                 QMessageBox.about(self, "INDEX ERROR", "The Rows and Columns Dimensions Are Invalid. Retry.")
+
+
+
+
 
             self.rowsText.clear()
             self.columnsText.clear()
+            self.componentsText.clear()
+            self.componentsText2.clear()
+            self.energyText.clear()
 
 
-            Mydataset = self.dataset[:, 1:]
+
+
             u, s, v, lra_chisq, ac_u, ac_v = doSVD(Mydataset)
             self.figure_svd.clear()
-            plot_svd_results(u, s, v, lra_chisq, ac_u, ac_v, self.figure_svd, Myenergy, n_cmp_show=3)
-            #plot_svd_results(u, s, v, lra_chisq, ac_u, ac_v, self.figure_svd, n_cmp_show=3)
+            plot_svd_results(u, s, v, lra_chisq, ac_u, ac_v, self.figure_svd, Myenergy, n_cmp_show=components)
             self.canvas.draw_idle()
             self.canvas_data.draw_idle()
 
