ec6ee025348c4eb5f80ed53d7c5347d3a6eed841
dleshchev <leshchev.denis@gmail.com>
2021-10-06 14:24:28 -0400

    added import button for the main widget_main.py added creation of Qt items and adding them to the dataset model, which then updates the listview

--- issfactortools/elements/mcrproject.py
+++ issfactortools/elements/mcrproject.py
@@ -1,86 +1,6 @@
 from pymcr.mcr import McrAR
 import numpy as np
 
-class MCRProject:
-
-    def __init__(self, data=None,
-                 data_ref = None,
-                 c_fix=None, st_fix=None,
-                 c_constraints=None, st_constraints=None,
-                 c_regr=None, st_regr=None, max_iter:int=1000):
-        self.data = data
-        self.data_ref = data_ref
-        self.c_fix = None
-        self.st_fix = None
-        self.c_constraints = c_constraints
-        self.st_constraints = st_constraints
-        self.c_regr = c_regr
-        self.st_regr = st_regr
-        self.max_iter = max_iter
-
-        if ((data is not None) and
-            (data_ref is not None) and
-            (c_fix is not None) and
-            (st_fix is not None) and
-            (c_constraints is not None) and
-            (st_constraints is not None) and
-            (c_regr is not None) and
-            (st_regr is not None)
-            (max_iter is not None)):
-
-            self.mcr = McrAR(st_regr=self.s_optmizer, c_regr=self.c_optimizer,
-                             c_constraints=self.c_constraints,
-                             st_constraints=self.st_constraints,
-                             max_iter=self.max_iter)
-
-    def fit(self):
-        self.mcr.fit(self.data.T, ST=self.data_ref.T,
-                     c_fix=self.c_fix,
-                     st_fix=self.st_fix)
-
-        self.data_ref_fit = self.mcr.ST_opt_.T
-        self.c_fit = self.mcr.C_opt_.T
-        self.data_fit = (self.data_ref_fit @ self.c_fit)
-
-
-
-
-
-def run_mcr_analysis(data_dict, fignum, key='data_flat'):
-    basis_set = data_dict['basis_set']
-    energy = data_dict['energy']
-    energy_svd_mask = data_dict['energy_svd_mask']
-    energy_svd = energy[energy_svd_mask]
-    data = data_dict[key][energy_svd_mask, :]
-    isSample = data_dict['sample_table']['isSample'].values
-    data_samples = data[:, isSample]
-
-    ###############
-    c_constraints = [ConstraintNorm(axis=-1), ConstraintCutBelow(0.00)]
-    st_constraints = [ConstraintCompressAbove(3.0)]  # , ConstraintCutBelow(0.07)]
-    st_fix = [0]
-    ST0 = basis_set.T
-
-    mcrar = McrAR(st_regr=NNLS(), c_regr=NNLS(),
-                  c_constraints=c_constraints,
-                  st_constraints=st_constraints,
-                  max_iter=int(1e3))
-    mcrar.fit(data_samples.T, ST=ST0, st_fix=st_fix)
-
-    data_dict['sample_table']['conc_Ce4+_mcr'] = np.zeros(isSample.size)
-    data_dict['sample_table']['conc_Ce3+_mcr'] = np.zeros(isSample.size)
-
-    data_dict['sample_table']['conc_Ce4+_mcr'][isSample] = mcrar.C_opt_[:, 0]
-    data_dict['sample_table']['conc_Ce3+_mcr'][isSample] = mcrar.C_opt_[:, 1]
-
-    data_dict['mcr_energy'] = energy_svd
-    data_dict['mcr_basis'] = basis_set
-    data_dict['mcr_spectra'] = mcrar.ST_opt_.T
-
-    data_fit_mcr = (mcrar.C_opt_ @ mcrar.ST_opt_).T
-
-#####
-
 
 class DataSet:
 
@@ -137,5 +57,68 @@ class DataSet:
     @property
     def t(self):
         return self.t[self.t_mask]
+
+
+
+class ReferenceSet:
+    pass
+
+
+class ConstraintSet:
+    pass
+
+
+
+
+class MCRProject:
+
+    def __init__(self, data=None,
+                 data_ref = None,
+                 c_fix=None, st_fix=None,
+                 c_constraints=None, st_constraints=None,
+                 c_regr=None, st_regr=None, max_iter:int=1000):
+        self.data = data
+        self.data_ref = data_ref
+        self.c_fix = None
+        self.st_fix = None
+        self.c_constraints = c_constraints
+        self.st_constraints = st_constraints
+        self.c_regr = c_regr
+        self.st_regr = st_regr
+        self.max_iter = max_iter
+
+        if ((data is not None) and
+            (data_ref is not None) and
+            (c_fix is not None) and
+            (st_fix is not None) and
+            (c_constraints is not None) and
+            (st_constraints is not None) and
+            (c_regr is not None) and
+            (st_regr is not None)
+            (max_iter is not None)):
+
+            self.mcr = McrAR(st_regr=self.s_optmizer, c_regr=self.c_optimizer,
+                             c_constraints=self.c_constraints,
+                             st_constraints=self.st_constraints,
+                             max_iter=self.max_iter)
+
+    def fit(self):
+        self.mcr.fit(self.data.T, ST=self.data_ref.T,
+                     c_fix=self.c_fix,
+                     st_fix=self.st_fix)
+
+        self.data_ref_fit = self.mcr.ST_opt_.T
+        self.c_fit = self.mcr.C_opt_.T
+        self.data_fit = (self.data_ref_fit @ self.c_fit)
+
+
+
+
+
+
+
+
+
+
 
 
--- issfactortools/widgets/widget_main.py
+++ issfactortools/widgets/widget_main.py
@@ -4,10 +4,12 @@ import numpy as np
 import pkg_resources
 import math
 
-from PyQt5 import uic, QtGui, QtCore
+from PyQt5 import uic, QtGui, QtCore, QtWidgets
 from PyQt5.QtCore import QThread, QSettings
 
 from issfactortools.widgets import widget_data_overview, widget_mcr_overview
+from issfactortools.elements.mcrproject import DataSet, ReferenceSet, ConstraintSet
+
 ui_path = pkg_resources.resource_filename('issfactortools', 'ui/ui_main.ui')
 
 
@@ -16,15 +18,66 @@ class FactorAnalysisGUI(*uic.loadUiType(ui_path)):
 
     progress_sig = QtCore.pyqtSignal()
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, parent=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
+        self.parent = parent
 
-        self.widget_data_overview = widget_data_overview.UIDataOverview()
-        self.layout_data_overview.addWidget(self.widget_data_overview)
+        self.push_import_dataset.clicked.connect(self.import_dataset)
+        self.model_datasets = QtGui.QStandardItemModel(self) # model that is used to show listview of datasets
+        self.model_references = QtGui.QStandardItemModel(self)  # model that is used to show listview of references
+        self.model_constraints = QtGui.QStandardItemModel(self)  # model that is used to show listview of constraints
 
-        self.widget_mcr_overview = widget_mcr_overview.UIDataOverview()
-        self.layout_mcr_analysis.addWidget(self.widget_mcr_overview)
+        #
+        # self.widget_data_overview = widget_data_overview.UIDataOverview()
+        # self.layout_data_overview.addWidget(self.widget_data_overview)
+        #
+        # self.widget_mcr_overview = widget_mcr_overview.UIDataOverview()
+        # self.layout_mcr_analysis.addWidget(self.widget_mcr_overview)
+
+    def _append_item_to_model(self, model, item):
+        parent = model.invisibleRootItem()
+        parent.appendRow(item)
+
+    def _make_item(self, name):
+        item = QtGui.QStandardItem(name)
+        item.setDropEnabled(False)
+        item.setCheckable(True)
+        item.setEditable(False)
+        return item
+
+    def _create_dataset(self, x, t, data, name='DataSet'):
+        item = self._make_item(name)
+        item.item_type = 'DataSet'
+        item.dataset = DataSet(x, t, data, name)
+        self._append_item_to_model(self.model_datasets, item)
+        self.listView_datasets.setModel(self.model_datasets)
+
+    # the two methods below are placeholders:
+
+    # def _create_reference_set(self, name='ReferenceSet'):
+    #     item = self._make_item(name)
+    #     item.item_type = 'ReferenceSet'
+    #     item.references = ReferenceSet()
+    #     self._append_item_to_model(self.model_references, item)
+    #     self.listView_references.setModel(self.model_references)
+    #
+    # def _create_constraint_set(self, name='ConstraintSet'):
+    #     item = self._make_item(name)
+    #     item.item_type = 'ConstraintSet'
+    #     item.constraints = ConstraintSet()
+    #     self._append_item_to_model(self.model_constraints, item)
+    #     self.listView_constraints.setModel(self.model_constraints)
+
+
+    def import_dataset(self):
+        filename = QtWidgets.QFileDialog.getOpenFileName(directory='/nsls2/xf08id/Sandbox',
+                                                         filter='*.xas', parent=self)[0]
+        filedata = np.genfromtxt(filename)
+        x = filedata[:, 0]
+        data = filedata[:, 1:]
+        t = np.arange(data.shape[1])
+        self._create_dataset(x, t, data, name=filename)
 
 
 
