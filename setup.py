a6f3be64488488b2f0a53675e08ead2bb9235dc9
Akhil Tayal <atayal@bnl.gov>
2026-09-19 10:41:55 -0400

    updated setup and MANIFEST file to allow ui files to be included in the package using pixi

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     include_package_data=True,
     package_data={
         'issfactortools': [
-            "issfactortools/ui/*ui",
+            "ui/*.ui",
             # When adding files here, remember to update MANIFEST.in as well,
             # or else they will not be included in the distribution on PyPI!
             # 'path/to/data_file',
